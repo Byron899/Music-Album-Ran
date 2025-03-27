1dda6c7801c7e48a18f79deb3129c5037ae74a7e
Byron899 <byronhughesr34@icloud.com>
2025-03-27 19:02:44 +0000

    Delete src directory

--- src/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import MusicRankingApp from './App';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<MusicRankingApp />);
