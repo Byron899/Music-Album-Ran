741d47b9eaad51ec86361ae83fb4a5a3065dd44f
Byron899 <byronhughesr34@icloud.com>
2025-03-27 17:36:51 +0000

    Add files via upload

---
+++ README.md
@@ -0,0 +1,2 @@
+# Music Album Ranking App
+Frontend for a music album ranking tool built in React.
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import MusicRankingApp from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<MusicRankingApp />);
