5c74de30e8691f0b4c1e7876ca0baa69cdfffed0
Byron899 <byronhughesr34@icloud.com>
2025-03-27 19:26:39 +0000

    src/

---
+++ final_src_corrected_paths/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import MusicRankingApp from "./App";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <MusicRankingApp />
+  </React.StrictMode>
+);
